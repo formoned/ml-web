63e8b7001dd5d1c10d3714feb49308dd76dbe94f
lapaartis <formoned@yahoo.com>
2018-04-13 02:18:28 +0300

    Autherization component init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Material Design Module*/
+import { MaterialModule } from './material.module';
+/* Angular Flex-Layout Module */
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginFormComponent } from './login-form/login-form.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    MaterialModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatCardModule, MatInputModule} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
+  ],
+})
+export class MaterialModule { }
